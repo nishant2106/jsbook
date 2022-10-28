56584b14135228071c3769f6018fd4a33acc2586
Nishant Pandey <59759332+nishant2106@users.noreply.github.com>
2022-10-28 21:20:03 +0530

    safely execution using sandbox complete

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,8 @@ import { fetchPlugin } from "./plugins/fetch-plugin";
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState("");
-  const [code, setCode] = useState("");
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -23,6 +23,7 @@ const App = () => {
     if (!ref.current) {
       return;
     }
+    iframe.current.srcdoc = html;
 
     const result = await ref.current.build({
       entryPoints: ["index.js"],
@@ -35,12 +36,29 @@ const App = () => {
       },
     });
 
-    setCode(result.outputFiles[0].text);
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
   };
   const html = `
-  <script>
-  ${code}
-  </script>`;
+  <html>
+    <head></head>
+    <body>
+      <div id="root"></div>
+      <script>
+        window.addEventListener('message', (event)=>{
+          try{
+          eval (event.data);
+          }
+          catch(err){
+            const root = document.querySelector('#root');
+            root.innerHTML ='<div style="color:red;"><h4>Runtime Error:</h4>' + err + '</div>';
+           console.error(err);
+          }
+        },false);
+      </script>
+    </body>
+  </html>
+  `;
 
   return (
     <div>
@@ -51,8 +69,12 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
-      <iframe title="sandbox" sandbox="allow-scripts" srcDoc={html}></iframe>
+      <iframe
+        ref={iframe}
+        title="sandbox"
+        sandbox="allow-scripts"
+        srcDoc={html}
+      ></iframe>
     </div>
   );
 };
