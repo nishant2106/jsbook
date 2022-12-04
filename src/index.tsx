cd6cd365c47a646d5114bf1007600732b8a00d42
Nishant Pandey <59759332+nishant2106@users.noreply.github.com>
2022-12-04 17:42:22 +0530

    17 Simplify State Updates with Immer

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,13 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { store } from "./state";
 import TextEditor from "./components/text-editor";
-import CodeEditor from "./components/code-editor";
+// import CodeEditor from "./components/code-editor";
 
 const App = () => {
   return (
     <Provider store={store}>
       <div>
-        <CodeEditor initialValue={"const a = 1"} onChange={() => {}} />
+        {/* <CodeEditor initialValue={"const a = 1"} onChange={() => {}} /> */}
         <TextEditor />
       </div>
     </Provider>
