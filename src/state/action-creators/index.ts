b5d5f2dd7ea3f2acbf6644ba1cf2d3dae20ec04a
Nishant Pandey <59759332+nishant2106@users.noreply.github.com>
2022-12-24 20:09:46 +0530

    19 Connecting Bundles in Redux

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -5,8 +5,11 @@ import {
   InsertCellAfterAction,
   MoveCellAction,
   UpdateCellAction,
+  Action,
 } from "../actions";
 import { CellTypes } from "../cell";
+import bundle from "../../bundler";
+import { Dispatch } from "redux";
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
   return {
@@ -35,6 +38,19 @@ export const insertCellAfter = (
 ): InsertCellAfterAction => {
   return {
     type: ActionType.INSERT_CELL_AFTER,
+
     payload: { id, type: cellType },
+  };
+};
+
+export const createBundle = (cellId: string, input: string) => {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({ type: ActionType.BUNDLE_START, payload: { cellId } });
+
+    const result = await bundle(input);
+    dispatch({
+      type: ActionType.BUNDLE_COMPLETE,
+      payload: { cellId, bundle: result },
+    });
   };
 };
